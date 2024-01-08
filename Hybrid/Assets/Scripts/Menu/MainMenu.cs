71ee79ec48acb8dcac07a113c065f57b874b7ab2
bora <oksal>
2024-01-08 22:46:25 +0100

    Made 2 scenes woohoo and updated gitignore

---
+++ Hybrid/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        //SceneManager.LoadScene("SceneName");
+    }
+    
+    public void LoadEndScene()
+    {
+       //SceneManager.LoadScene(EndScene);
+    }
+}
